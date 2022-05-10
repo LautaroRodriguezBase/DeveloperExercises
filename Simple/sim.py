d5b5bd5d0c79603e9955fe6ef19b9479d28d58e9
LautaroRB <lerb.dev@gmail.com>
2022-05-10 15:59:04 -0300

    Segunda parte del ejercicio terminada. Retorna una lista de diccionarios con la edad ordenada de mayor a menor

--- Simple/sim.py
+++ Simple/sim.py
@@ -2,14 +2,42 @@
 
 import random
 
+def CreateDicList():
+
+    dicList = [None] * 10
+
+    for i in range(0, len(dicList)):
+
+        dicList[i] = {'id':i+1, 'age': random.randint(1, 100)}
+
+    return dicList
+
+def SortList():
+
+    sortDicList = CreateDicList()
+    ageList = [None]*10
+
+    for i in range(0, len(sortDicList)):
+
+        ageList[i] = sortDicList[i]['age']
+
+    ageList.sort(reverse=False)
+
+    for age in ageList:
+
+        for i in range(0, len(ageList)):
+
+            if sortDicList[i]['age'] == age:
+
+                x = 0
+                sortDicList.insert(x, sortDicList[i])
+                ++x
+                sortDicList.pop(i+1)
+
+    return sortDicList
+
 def main():
 
-    listaDeDicc = [None] * 10
+    print(SortList())
 
-    for i in range(0, len(listaDeDicc)):
-
-        listaDeDicc[i] = {'id':i+1, 'age': random.randint(1, 100)}
-
-    return listaDeDicc
- 
 main()
