a6e4e8376a58afcba879c24c1dfb5c9eef5191fd
LautaroRB <lerb.dev@gmail.com>
2022-05-10 17:03:29 -0300

    Segunda parte del ejercicio, pequeñas mejoras y también printea el id de la persona más vieja y más joven

--- Simple/sim.py
+++ Simple/sim.py
@@ -2,22 +2,28 @@
 
 import random
 
+lenList = 10
+
 def CreateDicList():
 
-    dicList = [None] * 10
+    dicList = [None] * lenList
 
-    for i in range(0, len(dicList)):
+    for i in range(0, lenList):
 
         dicList[i] = {'id':i+1, 'age': random.randint(1, 100)}
 
     return dicList
 
-def SortList():
+def SortList(dicList):
 
-    sortDicList = CreateDicList()
-    ageList = [None]*10
+    idMayMen = """
+Id de la persona mayor: {}
+Id de la persona menor: {}
+"""
+    sortDicList = dicList
+    ageList = [None] * lenList
 
-    for i in range(0, len(sortDicList)):
+    for i in range(0, lenList):
 
         ageList[i] = sortDicList[i]['age']
 
@@ -25,7 +31,7 @@ def SortList():
 
     for age in ageList:
 
-        for i in range(0, len(ageList)):
+        for i in range(0, lenList):
 
             if sortDicList[i]['age'] == age:
 
@@ -34,10 +40,11 @@ def SortList():
                 ++x
                 sortDicList.pop(i+1)
 
+    print(idMayMen.format(sortDicList[0]['id'], sortDicList[9]['id']))
     return sortDicList
 
 def main():
 
-    print(SortList())
+    print(SortList(CreateDicList()))
 
 main()
