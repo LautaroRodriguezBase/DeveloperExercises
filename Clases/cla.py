3227e9b6edaebb62f79fc5eca2bec38573ae9a8b
LautaroRB <lerb.dev@gmail.com>
2022-05-12 15:34:50 -0300

    Permite la multiplicacion del circulo, creando un nuevo objeto cada vez que se lo multiplica

--- Clases/cla.py
+++ Clases/cla.py
@@ -4,7 +4,6 @@ import math
 
 class Circulo:
 
-
     def __init__(self, radio):
 
         self.radio = float(radio)
@@ -22,18 +21,23 @@ class Circulo:
 
 def radioEsValido(rad):
 
-    errorRadio = """Ups! El radio no debe:
+    errorRadio = """Ups! El numero no debe:
     -ser menor o igual a 0(cero) 
     -contener caracteres distintos de 0 a 9
     -o tener mas de un '.'(punto)"""
-    caracteresValidos = list((range(1, 10)))
+    caracteresValidos = list((range(0, 10)))
     caracteresValidos += "."
     val = False
 
     for char in rad:
 
         if (char in str(caracteresValidos)) and not(rad.count(".") > 1):
-            val = True
+
+            if float(rad) > 0:
+                val = True
+            else:
+                print(errorRadio)
+                return False    
         else:
             print(errorRadio)
             return False
@@ -41,27 +45,60 @@ def radioEsValido(rad):
     print("Calculando...")
     return val
             
-
 def crearObjeto(rad):
 
-    circulo1 = Circulo(rad)
+    circuloMul = 1
+    circuloNum = 0
+    circulos = []
 
-    circulo1.areaCirculo()
-    circulo1.perCirculo()
+    rad = float(rad)
 
-    if input("Desea cambiar el radio? y/n: ") == 'y':
+    circulos += [Circulo(rad)]
 
-        main()
+    circulos[circuloNum].areaCirculo()
+    circulos[circuloNum].perCirculo()
 
+    while True:
+
+        if input("Desea cambiar el radio? y/n: ") == 'y':
+
+            while True:
+                rad = input("Radio del circulo: ")
+                if radioEsValido(rad):
+
+                    circulos[circuloNum].radio = float(rad)
+                    circulos[circuloNum].areaCirculo()
+                    circulos[circuloNum].perCirculo()
+                    break
+                
+        elif input("Desea multiplicar el radio? y/n: ") == 'y':
+
+            circuloNum += 1
+            while True:
+                circuloMul = input("Circulo * ")
+                if radioEsValido(circuloMul):
+
+                    rad = (float(rad) * float(circuloMul))
+
+                    circulos += [Circulo(rad)]
+
+                    circulos[circuloNum].areaCirculo()
+                    circulos[circuloNum].perCirculo()
+                    break
+        else:
+            break
+
+    for circulo in circulos:
+        print(circulo.radio)
+        
 def main():
 
     radio = input("Radio del circulo: ")
 
     if radioEsValido(radio):
         
-        crearObjeto(float(radio))
+        crearObjeto(radio)
     else:
         main()
-
 
 main()
