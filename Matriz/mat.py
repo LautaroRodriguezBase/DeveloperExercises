eef349fde4d6d53d6af743e7516330a381085a37
LautaroRB <lerb.dev@gmail.com>
2022-05-16 01:10:15 -0300

    Se optimizó el código y se agregaron comentarios

--- Matriz/mat.py
+++ Matriz/mat.py
@@ -9,61 +9,34 @@ cantDeNumerosEnSecuencia = 4-1
 posFinal = []
 posInicial = []
 
-def scanHorizontal(m5x5):
+def scanHorizontal(m5x5, Desde, Hasta, Limite, paso):
 
     for filas in range(filasMatriz):
-        
-        for columnas in range(columnasMatriz-cantDeNumerosEnSecuencia):
-            x = 1
-            while m5x5[filas][columnas] == (m5x5[filas][columnas+x]-x):
-
-                if x == 3:
-            
-                    posInicial.insert(0, [filas, columnas])
-                    posFinal.insert(0, [filas, columnas+x])
-                    break
-                x+=1
     
-    for filas in range(filasMatriz):
-        
-        for columnas in range(columnasMatriz-1, (cantDeNumerosEnSecuencia-1), -1):
-            x = 1
-            while m5x5[filas][columnas] == (m5x5[filas][columnas-x]-x):
+        for columnas in range(Desde-1, Hasta-Limite, paso):
+            
+            x = int((paso**2)**0.5)
+            while m5x5[filas][columnas] == (m5x5[filas][columnas+(x*paso)]-x):
 
-                if x == 3:
+                if x == cantDeNumerosEnSecuencia:
             
                     posInicial.insert(0, [filas, columnas])
-                    posFinal.insert(0, [filas, columnas-x])
+                    posFinal.insert(0, [filas, columnas+(x*paso)])
                     break
                 x+=1
 
-def scanVertical(m5x5):
+def scanVertical(m5x5, Desde, Hasta, Limite, paso):
 
     for columnas in range(columnasMatriz):
+    
+        for filas in range(Desde-1, Hasta-Limite, paso):
+            x = int((paso**2)**0.5)
+            while m5x5[filas][columnas] == (m5x5[filas+(x*paso)][columnas]-x):
 
-        for filas in range(filasMatriz-cantDeNumerosEnSecuencia):
-
-            x = 1
-            while m5x5[filas][columnas] == ((m5x5[filas+x][columnas])-x):
-
-                if x == 3:
+                if x == cantDeNumerosEnSecuencia:
             
                     posInicial.insert(0, [filas, columnas])
-                    posFinal.insert(0, [filas+x, columnas])
-                    break
-                x+=1
-
-    for columnas in range(columnasMatriz):
-
-        for filas in range(filasMatriz-1, cantDeNumerosEnSecuencia-1, -1):
-
-            x = 1
-            while m5x5[filas][columnas] == ((m5x5[filas-x][columnas])-x):
-
-                if x == 3:
-            
-                    posInicial.insert(0, [filas, columnas])
-                    posFinal.insert(0, [filas-x, columnas])
+                    posFinal.insert(0, [filas+(x*paso), columnas])
                     break
                 x+=1
 
@@ -71,8 +44,17 @@ def main():
 
     matriz = R.random.randint(1, 6, size=(filasMatriz, columnasMatriz))
 
-    scanHorizontal(matriz)
-    scanVertical(matriz)
+    # Se envia:
+        # m5x5 = la matriz,
+        # Desde = el numero desde que parte,
+        # Hasta = el numero hasta el que va,
+        # Limite = el numero que se le resta al anterior para que la matriz no se salga de rango,
+        # Paso = el paso para recorrer la matriz
+    scanHorizontal(matriz, 1, columnasMatriz, cantDeNumerosEnSecuencia, 1)
+    scanHorizontal(matriz, columnasMatriz, cantDeNumerosEnSecuencia, 1, -1)
+
+    scanVertical(matriz, 1, filasMatriz, cantDeNumerosEnSecuencia, 1)
+    scanVertical(matriz, filasMatriz, cantDeNumerosEnSecuencia, 1, -1)
     
     print("""
 {}
